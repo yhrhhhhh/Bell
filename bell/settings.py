42842c866d707c3c38fb579e2ff85c0d172c9a2d
Exoa <yanhuiru81@gmail.com>
2025-06-13 16:36:06 +0800

    Fix deployment configuration files

--- bell/settings.py
+++ bell/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'bell'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'dcny123'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'dcny@123'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306')
     }
